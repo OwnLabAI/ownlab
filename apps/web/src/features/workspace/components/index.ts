09a4e7cfd1df20c2e7b30f072af0a2157ded6d93
Xilang Zhou <xlzhou.long@gmail.com>
2026-04-04 00:26:32 +0800

    fix: delete dead code in workspace

--- apps/web/src/features/workspace/components/index.ts
+++ apps/web/src/features/workspace/components/index.ts
@@ -1,7 +1,5 @@
 export { WorkspaceContainer } from './workspace-container';
 export { ToolPanel } from './tool-panel';
 export { WorkspaceSwitcher } from './workspace-switcher';
-export { WorkspaceNavItems } from './workspace-nav-items';
-export { NavNew } from './nav-new';
 export { WorkspaceDefaultView } from './workspace-default-view';
 export { ChannelChat } from './channel';
